8a7b99d0741569f041cc0b7558096852af4e6bb5
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-11-08 12:41:15 +0100

    styling

--- src/App.vue
+++ src/App.vue
@@ -117,7 +117,7 @@ const nextQuestion = () => {
             : 'next Question' }}
       </button>
     </section>
-    
+
     <section v-else>
       <h2>you have finished the quiz</h2>
       <p>your score is {{ score }}  {{ questions.length }}</p>
@@ -136,5 +136,110 @@ const nextQuestion = () => {
 body {
   background-color: #271c36;
   color: #fff;
+}
+
+.app{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  min-height: 100vh;
+}
+
+h1{
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+
+.quiz{
+  background-color: #382a4b;
+  padding: 1rem;
+  width: 100%;
+  max-width: 640px;
+  border-radius: 0.5rem;
+}
+
+.quiz-info{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+
+.quiz-info .question{
+  color: #8f8f8f;
+  font-size: 1.5rem;
+}
+
+.quiz-info .score{
+  color: #fff;
+  font-size: 1.25rem;
+}
+
+.options {
+  margin-bottom: 1rem;
+}
+
+.option {
+  padding: 1rem;
+  display: block;
+  background-color: #271c36;
+  margin-bottom: 0.5rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+}
+
+.option:hover{
+  background-color: #2d213f;
+}
+
+.option.correct{
+  background-color: #2cce7d;
+}
+
+.option.wrong {
+  background-color: #ff5a5f;
+}
+
+.option:last-of-type{
+  margin-bottom: 0;
+}
+
+.option.disabled {
+  opacity: 0.5;
+}
+
+.option input {
+  display: none;
+}
+
+button {
+  appearance: none;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  padding: 0.5rem 1rem;
+  background-color: #2cce7d;
+  color: #2d213f;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 1.25rem;
+  border-radius: 0.5rem;
+}
+
+button:disabled{
+  opacity: 0.5;
+}
+
+h2 {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+p {
+   color: #8f8f8f;
+   font-size: 1.25rem;
+   text-align: center;
 }
 </style>
