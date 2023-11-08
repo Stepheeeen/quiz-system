1f2f852773eb2c0f649eb6054e81b180a97d1ca0
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-11-08 12:23:24 +0100

    structure and functionality

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, computed} from 'vue'
+import { ref, computed } from 'vue'
 
 const questions = ref([
   {
@@ -45,23 +45,95 @@ const questions = ref([
       'A framework for building user interfaces'
     ],
     selected: null
-  },
-
+  }
 ])
+
+const quizCompleted = ref(false)
+
+const currQuestion = ref(0)
+const score = computed(() => {
+  let value = 0
+  questions.value.map(q => {
+    if (q.selected == q.answer) {
+      value++
+    }
+  })
+  return value
+})
+
+const getCurrQuestion = computed(() => {
+  let question = questions.value[currQuestion.value]
+  question.index = currQuestion.value
+  return question
+})
+
+const setAnswer = e => {
+  questions.value[currQuestion.value].selected = evt.target.value
+  evt.target.value = null
+}
+
+const nextQuestion = () => {
+  if (currQuestion.value < questions.value.length - 1) {
+    currQuestion.value++
+  } else {
+    quizCompleted.value = true
+  }
+}
 </script>
-
+  
 <template>
+  <main class="app">
+    <h1>The Quiz</h1>
 
+    <section class="quiz" v-if="!quizCompleted">
+      <div class="quiz-info">
+        <span class="question">{{ getCurrQuestion.question }}</span>
+        <span class="score">score {{ score }}/{{ questions.length }}</span>
+      </div>
+
+      <div class="options">
+        <label v-for="(option, index) in getCurrQuestion.options" :key="index" :class="`option ${getCurrQuestion.selected == index
+          ? index == getCurrQuestion.answer
+            ? 'correct'
+            : 'wrong'
+          : ''
+
+          } ${getCurrQuestion.selected != null &&
+            index != getCurrQuestion.selected
+            ? 'disabled'
+            : ''
+          }`">
+          <input type="radio" :name="getCurrQuestion.index" :value="index" v-model="getCurrQuestion.selected"
+            :disabled="getCurrQuestion.selected" @change="setAnswer">
+          <span>{{ option }}</span>
+        </label>
+      </div>
+
+      <button @click="nextQuestion" :disabled="!getCurrQuestion.selected">
+        {{ getCurrQuestion.index == questions.length - 1
+          ? 'finish'
+          : getCurrQuestion.selected == null
+            ? 'select an option'
+            : 'next Question' }}
+      </button>
+    </section>
+    
+    <section v-else>
+      <h2>you have finished the quiz</h2>
+      <p>your score is {{ score }}  {{ questions.length }}</p>
+    </section>
+  </main>
 </template>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: 'Montserrat', sans-serif;
 }
-body{
+
+body {
   background-color: #271c36;
   color: #fff;
 }
