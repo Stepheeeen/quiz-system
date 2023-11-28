ef60e4659118bf657065e009ca5d6d64142b252a
Steph4n <108176326+Stepheeeen@users.noreply.github.com>
2023-11-28 21:31:52 +0000

    router testing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import PageOne from './component/PageOne.vue'
+// import Home from '../src/App.vue'
+
+Vue.use(VueRouter)
+
+const Routes = [
+    {
+        path: '/',
+        component: App
+    },
+    {
+        path: '/Question-1',
+        component: PageOne
+    }
+]
 
 createApp(App).mount('#app')
